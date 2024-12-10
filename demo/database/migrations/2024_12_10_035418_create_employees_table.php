455d408a9a75fad7b23594bb557d61bf1da14c38
thuytiendev <thuytien16102003@gmail.com>
2024-12-10 11:22:30 +0700

    change

--- demo/database/migrations/2024_12_10_035418_create_employees_table.php
+++ demo/database/migrations/2024_12_10_035418_create_employees_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('employees', function (Blueprint $table) {
             $table->id();  // ID cho nhân viên
             $table->string('name');  // Tên nhân viên
-            $table->string('email')->unique();  // Email nhân viên
+            $table->string('email', 191)->unique();  // Email nhân viên
             $table->string('password');  // Mật khẩu nhân viên
             $table->tinyInteger('role')->default(1);  // Thêm trường role với giá trị mặc định là 1 (user), 0 là admin
             $table->timestamps();  // Thời gian tạo và cập nhật
